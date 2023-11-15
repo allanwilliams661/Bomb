65c6250c3044d0a248916754936202add65626f7
allanwilliams661 <148219159+allanwilliams661@users.noreply.github.com>
2023-11-14 22:42:55 -0500

    speed
    Set up sprite speed = 6.

--- cutters.py
+++ cutters.py
@@ -6,8 +6,10 @@ class Cutters(pygame.sprite.Sprite):
         super().__init__()
         self.rect = pygame.Rect(x,y, cutter_width, cutter_height)
         self.image = pygame.image.load('wire_cut.png')
-        self.image = pygame.transform.scale(self.image,0.2)
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 6
         # cutters start in the middle of the screen
         self.rect.x = screen.get_width()/2
         self.rect.y = screen.get_width()/2
